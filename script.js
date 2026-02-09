eb53319e2bc3a1e8f6813f46873bfb577fc70e9f
mibarndageoffrey2045-creator <mibarndageoffrey2045@gmail.com>
2026-02-09 10:46:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Slideshow
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
+
+// Sidebar toggle
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
